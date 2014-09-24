911486df2a5e6451cb392809438e4b3d080d9ad5
Jiwen 'Steve' Cai <jwcai@me.com>
2014-09-24 13:14:36 -0700

    clean up xmonad config to fit gnome

--- jwcai/managed_files/xmonad.hs
+++ jwcai/managed_files/xmonad.hs
@@ -1,41 +1,18 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Config.Gnome
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout
-import System.IO
+import XMonad.Hooks.ManageDocks
 
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
-    ]
-
--- myLayout = Tall 1 (1/12) (1/2) ||| Full
 myLayout = Tall 1 (1/12) (1/2) ||| ThreeCol 1 (1/12) (1/3) ||| Full
 
-orLayout = layoutHook defaultConfig
-
 main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
+    xmonad $ gnomeConfig
         {
-        -- make sure to include myManageHook definition from above
-        manageHook = manageDocks <+> myManageHook
-                        <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $ myLayout
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+        terminal = "terminator"
         , borderWidth = 1
         , normalBorderColor = "#1C1C1C"
         , focusedBorderColor = "#C61915"
-        , terminal = "terminator"
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
+        , layoutHook = avoidStruts  $ myLayout
+        }
